726ca5f493d2727f8fdd57cbf6b863c177aa9831
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-06-24 16:44:33 -0400

    Updates

---
+++ precache-manifest.59fab0f50728402d9516d274978936f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5b51fe2f090f8ab895338230371751",
+    "url": "/monsters-page/index.html"
+  },
+  {
+    "revision": "57358bc0d3651dbfc716",
+    "url": "/monsters-page/static/css/main.c18de821.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/monsters-page/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-page/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57358bc0d3651dbfc716",
+    "url": "/monsters-page/static/js/main.f47c5d89.chunk.js"
+  },
+  {
+    "revision": "bcdc2851871041bfc0ac",
+    "url": "/monsters-page/static/js/runtime-main.0f32fee9.js"
+  }
+]);
